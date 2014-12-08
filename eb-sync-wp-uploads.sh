0f39fa0866b0da42ef23ae077ca5b6bc8de1c446
Alex Goretoy <alex@goretoy.com>
2014-12-07 22:04:00 -0800

    Update eb-sync-wp-uploads.sh
    Updated script to handle getting the keypair to use from the instance, all instances should be using the same keypair but left inside loop just in case something funky

--- eb-sync-wp-uploads.sh
+++ eb-sync-wp-uploads.sh
@@ -3,7 +3,6 @@
 # ElasticBeanstalk helpers script to sync all uploads in a Wordpress VCCW deployed environment
 
 EC2_USER=ec2-user
-AWS_KEY=~/.ssh/VOC-us-east-1-aws-eb
 UPLOADS_DIR=wp-content/uploads
 INSTANCE_DIR=/var/www/html/$UPLOADS_DIR/
 LOCAL_DIR=www/wordpress/$UPLOADS_DIR/
@@ -26,14 +25,16 @@ function syncDirectory(){
 function syncAllInstancesDirectories(){
     for instance in `eb status --verbose|grep InService|awk '{print $1}'`; do
         instance=${instance//:/};
+
+        AWS_KEY=~/.ssh/"`ec2-describe-instances $instance|grep INSTANCE|awk '{print $7}'`";
+        if [ ! -f "$AWS_KEY" ]; then
+            echo "$AWS_KEY keypair not found, cannot sync";
+            exit 1;
+        fi
+
         EC2_INSTANCE_URL=`ec2-describe-instances $instance|grep -i 'privateipaddress'|awk '{print $4}'`;
         syncDirectory
     done
 }
 
-if [ "$1" != "" ]; then
-    EC2_INSTANCE_URL="$1"
-    syncDirectory
-else
-    syncAllInstancesDirectories
-fi
+syncAllInstancesDirectories
